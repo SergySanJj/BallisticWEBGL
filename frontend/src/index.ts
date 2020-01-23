51f29e7c8a607e2e02853e688a745132a1cf8d32
SergySanJj <isara.isara8@gmail.com>
2020-01-23 23:45:27 +0200

    Add basic styles and gui

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,5 +1,22 @@
+const gravitySlider = document.getElementById("gravity-slider") as HTMLInputElement;
+gravitySlider.addEventListener('input', updateGui);
+const gravityDisplay = document.getElementById("gravity-display");
+
+export function updateGui() {
+    const newGravity = gravitySlider.value;
+    gravityDisplay.innerHTML = newGravity;
+}
+
+export function clearDisplay() {
+
+}
+
+export function fireProjectile() {
+
+}
 
 
+updateGui();
 
 const canvas = document.querySelector('canvas');
 const gl = canvas.getContext("webgl");
@@ -9,3 +26,6 @@ const gl = canvas.getContext("webgl");
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 // Clear the color buffer with specified clear color
 gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+
